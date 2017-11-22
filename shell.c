ac7cbcff88b83cff762f90cbba9bd120ccc99d20
Mansour <melsharawy@stuy.edu>
2017-11-22 10:58:45 -0500

    semicolons but no spaces

---
+++ shell.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+void exec_args (char * line){
+  char * line2 = (char*)malloc(200);
+  char * line3 = strcpy(line2, line);
+  char ** arr = (char**)malloc(6 * sizeof(char*));
+  int i = 0;
+  while(line3){
+    arr[i] = strsep(&line3," ");
+    i++;
+  }
+  arr[i] = NULL;
+  int f = fork();
+  if(!f){
+  execvp(arr[0],arr);
+  }
+}
+
+void multi_exec (char * line){
+  char * line2 = (char*)malloc(200);
+  char * line3 = strcpy(line2, line);
+  int status;
+  while(line3){
+    exec_args(strsep(&line3,";"));
+    wait(&status);
+  }
+}
+
+int main(){
+  /*char line[100] = "wow-this-is-cool";
+  char *s1 = line;
+  printf("[%s]\n", strsep( &s1, "-" ));
+  printf("[%s]\n", s1);*/
+  multi_exec("echo hi;echo herman");
+  return 0;
+}
