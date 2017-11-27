7c1ec20d98ba13cd58d4970105d38f350a44f5d4
Razorflame3912 <melsharawy@stuy.edu>
2017-11-26 19:10:11 -0500

    added basic shell input and exit functionality, spacing with semicolons still to be fixed

--- shell.c
+++ shell.c
@@ -14,6 +14,9 @@ void exec_args (char * line){
   char * line3 = strcpy(line2, line);
   char ** arr = (char**)malloc(6 * sizeof(char*));
   int i = 0;
+  if(strncmp(line3,"exit",4) == 0){
+    exit(0);
+  }
   while(line3){
     arr[i] = strsep(&line3," ");
     i++;
@@ -40,6 +43,12 @@ int main(){
   char *s1 = line;
   printf("[%s]\n", strsep( &s1, "-" ));
   printf("[%s]\n", s1);*/
-  multi_exec("echo hi;echo herman");
+  while(1){
+    char * input = (char *)malloc(200);
+    printf("CShell: ");
+    scanf(" %[^\t\n]s", input);
+    //printf("[%s]\n", input);
+    multi_exec(input);
+  }
   return 0;
 }
