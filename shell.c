7d85c1a40004622075a9b0ec69eb21cc932dec0d
Razorflame3912 <melsharawy@stuy.edu>
2017-11-27 21:08:03 -0500

    Made new strsep method that takes string as delim, will replace

--- shell.c
+++ shell.c
@@ -9,6 +9,26 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+char * strsepstr(char ** s, char * delim){
+  char * found = strstr(*s,delim);
+  //printf("[%s]",found);
+  char * ret = *s;
+  if(!found){
+    *s = NULL;
+    return ret;
+  }
+  printf("%s\n",found);
+  int i = 0;
+  for(i;i<strlen(delim);i++){
+    found[i] = NULL;
+  }
+  i+=1;
+  i -= 1;
+  *s = found + i;
+  return ret;
+  
+}
+
 void exec_args (char * line){
   char * line2 = (char*)malloc(200);
   char * line3 = strcpy(line2, line);
@@ -38,17 +58,33 @@ void multi_exec (char * line){
   }
 }
 
+
+
 int main(){
-  /*char line[100] = "wow-this-is-cool";
+  char line[100] = "wow-this-is-cool";
   char *s1 = line;
-  printf("[%s]\n", strsep( &s1, "-" ));
-  printf("[%s]\n", s1);*/
-  while(1){
+  printf("[%s]\n", strsep( &s1, "z" ));
+  printf("[%s]\n", s1);
+
+  char line2[100] = "ls -a -l ; woah";
+  char *s2 = line2;
+  printf("[%s]\n", strsepstr( &s2, " ; " ));
+  printf("[%s]\n", s2);
+  /*while(1){
+    int status;
     char * input = (char *)malloc(200);
-    printf("CShell: ");
+    int dir = fork();
+    if(!dir){
+      execlp("pwd","pwd",NULL);
+    }
+    else{
+      wait(&status);
+    }
+    printf("CShell$ ");
     scanf(" %[^\t\n]s", input);
     //printf("[%s]\n", input);
     multi_exec(input);
   }
+  */
   return 0;
 }
